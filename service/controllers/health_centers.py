a21c9346cbb9beed64ec1e21bbf6ad5c3b0f7dee
Emily Claps <108016846+emilykathryn@users.noreply.github.com>
2023-07-05 12:16:55 -0700

    added sample json file to service/data containing Senegal health centers, as well as health_centers controller adding an API to access the data in the health_centers file.

---
+++ service/controllers/health_centers.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+from flask_restful import Resource
+from rse_api.decorators import schema_out, register_resource
+
+
+@register_resource(['/health_centers'])
+class HealthCenters(Resource):
+    def get(self):
+        """
+        Example 1: /health_centers
+        return:
+            {
+                "Senegal": {
+                    "Health Center A": {
+                        "lat": 14.716677,
+                        "long": -17.467686
+                    },
+                    "Health Center B": {
+                        "lat": 15.6142,
+                        "long": -16.2287
+                    }
+                }
+            }
+        """
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        file = os.path.join(current_dir, '..', 'data', 'health_centers.json')
+        with open(file, 'r') as f:
+            health_centers = json.load(f)
+        return health_centers
+
