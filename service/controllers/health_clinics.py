84600c5bbc778c77929c9025e2e68f119aa609e7
Emily Claps <108016846+emilykathryn@users.noreply.github.com>
2023-07-11 11:43:16 -0700

    cleaned up health_clinics API, added events example file and API

--- service/controllers/health_clinics.py
+++ service/controllers/health_clinics.py
@@ -2,14 +2,15 @@ import json
 import os
 
 from flask_restful import Resource
-from rse_api.decorators import schema_out, register_resource
+from rse_api.decorators import register_resource
+from service.helpers.controller_helpers import ControllerException
 
 
-@register_resource(['/health_centers'])
-class HealthCenters(Resource):
+@register_resource(['/health_clinics'])
+class HealthClinics(Resource):
     def get(self):
         """
-        Example 1: /health_centers
+        Example 1: /health_clinics
         return:
             {
                 "Senegal": {
@@ -24,9 +25,13 @@ class HealthCenters(Resource):
                 }
             }
         """
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        file = os.path.join(current_dir, '..', 'data', 'health_clinics.json')
-        with open(file, 'r') as f:
-            health_centers = json.load(f)
-        return health_centers
+        try:
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            file = os.path.join(current_dir, '..', 'data', 'health_clinics.json')
+            with open(file, 'r') as f:
+                health_clinics = json.load(f)
+            return health_clinics
+
+        except ControllerException as e:
+            return str(e), 400
 
