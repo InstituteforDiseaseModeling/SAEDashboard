3078d0d7871d5b962f9a5645471da0bd2a421cda
Emily Claps <108016846+emilykathryn@users.noreply.github.com>
2023-09-26 10:11:09 -0700

    updated controllers, schemas, app.py with fast-api format. updated requirements_dev.

--- service/controllers/Test.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-from rse_api.decorators import register_resource
-
-
-@register_resource(['/'])
-class MapGeoData(Resource):
-    def get(self):
-        return "Hello World!"
