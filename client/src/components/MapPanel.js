1e7ac665f7d3351e956edd1bbbb527db643a74f7
David Kong <david.kong@gatesfoundation.org>
2024-07-19 15:01:40 -0700

    for fixing refresh issue

--- client/src/components/MapPanel.js
+++ client/src/components/MapPanel.js
@@ -74,7 +74,7 @@ const MapPanel = (props) => {
 
   useEffect(()=>{
     changeSubgroup(null);
-  }, [indicators.length]);
+  }, [indicators.length, selectedIndicatorRedux]);
 
   useEffect(()=>{
     changeSubgroup(null);
